a77aa5ae1f924f4f641fce1c1cc420f3347382d5
Andriy Savchenko <andriy.s.savchenko@gmail.com>
2016-11-13 00:10:10 +0200

    Added ActivityComponent

---
+++ app/src/main/java/ua/anironglass/template/injection/components/ActivityComponent.java
@@ -0,0 +1,17 @@
+package ua.anironglass.template.injection.components;
+
+import dagger.Subcomponent;
+import ua.anironglass.template.injection.PerActivity;
+import ua.anironglass.template.injection.modules.ActivityModule;
+import ua.anironglass.template.ui.activities.main.MainActivity;
+
+/**
+ * This component inject dependencies to all Activities across the application
+ */
+@PerActivity
+@Subcomponent(modules = ActivityModule.class)
+public interface ActivityComponent {
+
+    void inject(MainActivity mainActivity);
+
+}
