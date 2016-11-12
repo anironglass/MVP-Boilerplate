b73cc4627e00cb343af0609f208181aeaf2940b3
Andriy Savchenko <andriy.s.savchenko@gmail.com>
2016-11-12 22:49:25 +0200

    Added custom Application

---
+++ app/src/main/java/ua/anironglass/template/App.java
@@ -0,0 +1,12 @@
+package ua.anironglass.template;
+
+import android.app.Application;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
