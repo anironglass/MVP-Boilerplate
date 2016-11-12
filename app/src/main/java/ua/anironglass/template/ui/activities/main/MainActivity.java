60fc4831815a8009b5e75cdb336d27588623915b
Andriy Savchenko <andriy.s.savchenko@gmail.com>
2016-11-13 00:27:00 +0200

    Injected components to MainActivity

--- app/src/main/java/ua/anironglass/template/ui/activities/main/MainActivity.java
+++ app/src/main/java/ua/anironglass/template/ui/activities/main/MainActivity.java
@@ -2,14 +2,47 @@ package ua.anironglass.template.ui.activities.main;
 
 import android.os.Bundle;
 
+import javax.inject.Inject;
+
+import timber.log.Timber;
 import ua.anironglass.template.R;
 import ua.anironglass.template.ui.activities.base.BaseActivity;
 
 public class MainActivity extends BaseActivity implements MainMvpView {
 
+    @Inject MainPresenter mainPresenter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Timber.d("Create MainActivity");
+        getComponent().inject(this);
         setContentView(R.layout.activity_main);
+
+        initializeView();
     }
+
+    @Override
+    public void onResume() {
+        Timber.i("Resume MainActivity");
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        Timber.i("Pause MainActivity");
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Timber.i("Destroy MainActivity");
+        super.onDestroy();
+        mainPresenter.detachView();
+    }
+
+    private void initializeView() {
+        mainPresenter.attachView(this);
+    }
+
 }
--- app/src/main/java/ua/anironglass/template/ui/activities/main/MainPresenter.java
+++ app/src/main/java/ua/anironglass/template/ui/activities/main/MainPresenter.java
@@ -1,11 +1,18 @@
 package ua.anironglass.template.ui.activities.main;
 
 
+import javax.inject.Inject;
+
 import timber.log.Timber;
+import ua.anironglass.template.injection.ConfigPersistent;
 import ua.anironglass.template.ui.activities.base.BasePresenter;
 
+@ConfigPersistent
 class MainPresenter extends BasePresenter<MainMvpView> {
 
+    @Inject
+    MainPresenter() {
+    }
 
     @Override
     public void attachView(MainMvpView mvpView) {
