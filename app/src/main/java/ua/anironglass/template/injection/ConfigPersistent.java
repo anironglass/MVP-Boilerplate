d7919793b20d110f5e6b153d6f9aeab664fb353f
Andriy Savchenko <andriy.s.savchenko@gmail.com>
2016-11-13 00:12:57 +0200

    Added ConfigPersistentComponent

---
+++ app/src/main/java/ua/anironglass/template/injection/ConfigPersistent.java
@@ -0,0 +1,18 @@
+package ua.anironglass.template.injection;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Scope;
+
+import ua.anironglass.template.injection.components.ConfigPersistentComponent;
+
+
+/**
+ * A scoping annotation to permit dependencies conform to the life of the
+ * {@link ConfigPersistentComponent}
+ */
+@Scope
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ConfigPersistent {
+}
