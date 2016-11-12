fef7c8b50f3fca7549f7342252587de4b25b9e92
Andriy Savchenko <andriy.s.savchenko@gmail.com>
2016-11-12 22:50:15 +0200

    Initialize Timber

--- app/src/main/java/ua/anironglass/template/App.java
+++ app/src/main/java/ua/anironglass/template/App.java
@@ -2,11 +2,21 @@ package ua.anironglass.template;
 
 import android.app.Application;
 
+import timber.log.Timber;
+
 public class App extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        initializeTimber();
+    }
+
+    private void initializeTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
     }
 
 }
