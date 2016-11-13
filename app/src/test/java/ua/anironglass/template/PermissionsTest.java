ecd6798c187da3923cbd3374f389519816ea77f7
Andriy Savchenko <andriy.s.savchenko@gmail.com>
2016-11-13 03:23:05 +0200

    Added PermissionsTest

---
+++ app/src/test/java/ua/anironglass/template/PermissionsTest.java
@@ -0,0 +1,38 @@
+package ua.anironglass.template;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+import org.robolectric.manifest.AndroidManifest;
+import org.robolectric.res.Fs;
+
+import ua.anironglass.template.utils.DefaultConfig;
+
+import static com.google.common.truth.Truth.assertThat;
+
+
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest = Config.NONE, sdk = DefaultConfig.EMULATE_SDK)
+public final class PermissionsTest {
+
+    private static final Object[] EXPECTED_PERMISSIONS = {
+            "android.permission.INTERNET"
+    };
+
+    private static final String MERGED_MANIFEST_FILE =
+            "build/intermediates/manifests/full/debug/AndroidManifest.xml";
+
+    @Test
+    public void shouldMatchPermissions() {
+        AndroidManifest manifest = new AndroidManifest(
+                Fs.fileFromPath(MERGED_MANIFEST_FILE),
+                null,
+                null
+        );
+
+        assertThat(manifest.getUsedPermissions())
+                .containsExactly(EXPECTED_PERMISSIONS);
+    }
+
+}
