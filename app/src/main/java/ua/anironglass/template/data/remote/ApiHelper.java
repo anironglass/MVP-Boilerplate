339582c5445166daf667525835533cac62610a12
Andriy Savchenko <andriy.s.savchenko@gmail.com>
2016-11-14 13:21:35 +0200

    Implemented api call

--- app/src/main/java/ua/anironglass/template/data/remote/ApiHelper.java
+++ app/src/main/java/ua/anironglass/template/data/remote/ApiHelper.java
@@ -1,10 +1,21 @@
 package ua.anironglass.template.data.remote;
 
 
+import android.support.annotation.NonNull;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
 
+import rx.Observable;
+import rx.schedulers.Schedulers;
+import timber.log.Timber;
+import ua.anironglass.template.data.local.PreferencesHelper;
+import ua.anironglass.template.data.model.Photo;
 import ua.anironglass.template.injection.ApiServiceInstance;
+import ua.anironglass.template.utils.LogHelper;
 
 @Singleton
 public class ApiHelper {
@@ -13,11 +24,27 @@ public class ApiHelper {
     private static final int TIMEOUT_IN_SECONDS = 15;
 
     private ApiService mApiService;
+    private PreferencesHelper mPreferencesHelper;
 
     @Inject
     @SuppressWarnings("WeakerAccess")  // Used in global singleton
-    public ApiHelper(@ApiServiceInstance ApiService apiService) {
+    public ApiHelper(@ApiServiceInstance ApiService apiService,
+                     @NonNull PreferencesHelper preferencesHelper) {
         mApiService = apiService;
+        mPreferencesHelper = preferencesHelper;
+
+    }
+
+    @NonNull
+    public Observable<List<Photo>> getPhotos() {
+        int albumId = mPreferencesHelper.getAlbumId();
+        return mApiService.getPhotos(albumId)
+                .subscribeOn(Schedulers.io())
+                .doOnNext(photos -> Timber.d(
+                        LogHelper.attachThreadName("Loaded remote photos, albumId = %d"),
+                        albumId))
+                .retry(RETRY_COUNT_FOR_REQUEST)
+                .timeout(TIMEOUT_IN_SECONDS, TimeUnit.SECONDS);
     }
 
 }
--- app/src/main/java/ua/anironglass/template/data/remote/ApiService.java
+++ app/src/main/java/ua/anironglass/template/data/remote/ApiService.java
@@ -5,20 +5,28 @@ import android.support.annotation.NonNull;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import rx.Observable;
+import ua.anironglass.template.data.model.Photo;
 import ua.anironglass.template.utils.AutoValueGsonTypeAdapterFactory;
 
 
 public interface ApiService {
 
+    @GET("albums/{albumId}/photos")
+    Observable<List<Photo>> getPhotos(@Path("albumId") int albumId);
+
     class Builder {
 
-        private static final String BASE_URL = "...";
+        private static final String BASE_URL = "https://jsonplaceholder.typicode.com/";
         private static final int CONNECT_TIMEOUT = 15;
         private static final int WRITE_TIMEOUT = 20;
         private static final int READ_TIMEOUT = 20;
