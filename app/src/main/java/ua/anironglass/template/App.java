5574599598f9ccea885bd1e325ed1ab51821fb67
Andriy Savchenko <andriy.s.savchenko@gmail.com>
2017-01-02 21:30:18 +0200

    Improve Debug App

--- app/src/main/java/ua/anironglass/template/App.java
+++ app/src/main/java/ua/anironglass/template/App.java
@@ -2,11 +2,9 @@ package ua.anironglass.template;
 
 import android.app.Application;
 import android.content.Context;
-import android.os.StrictMode;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
-import timber.log.Timber;
 import ua.anironglass.template.injection.components.ApplicationComponent;
 import ua.anironglass.template.injection.components.DaggerApplicationComponent;
 import ua.anironglass.template.injection.modules.ApplicationModule;
@@ -14,14 +12,6 @@ import ua.anironglass.template.injection.modules.ApplicationModule;
 public class App extends Application {
 
     private ApplicationComponent mApplicationComponent;
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-
-        initializeStrictMode();
-        initializeTimber();
-    }
 
     @NonNull
     public static App get(@NonNull Context context) {
@@ -40,26 +30,6 @@ public class App extends Application {
     // Needed to replace the component with a test specific one
     public void setComponent(@Nullable ApplicationComponent applicationComponent) {
         mApplicationComponent = applicationComponent;
-    }
-
-    private void initializeStrictMode() {
-        if (BuildConfig.DEBUG) {
-            StrictMode.ThreadPolicy threadPolicy = new StrictMode.ThreadPolicy.Builder()
-                    .detectAll()
-                    .build();
-            StrictMode.setThreadPolicy(threadPolicy);
-
-            StrictMode.VmPolicy vmPolicy = new StrictMode.VmPolicy.Builder()
-                    .detectAll()
-                    .build();
-            StrictMode.setVmPolicy(vmPolicy);
-        }
-    }
-
-    private void initializeTimber() {
-        if (BuildConfig.DEBUG) {
-            Timber.plant(new Timber.DebugTree());
-        }
     }
 
 }
