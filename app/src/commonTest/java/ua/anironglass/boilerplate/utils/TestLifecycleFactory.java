0ffabc4bec9c1faf64df89f0dba3b384b56cb65f
Andriy Savchenko <andriy.s.savchenko@gmail.com>
2017-01-05 13:18:22 +0200

    Refactored TestLifecycleFactory

--- app/src/commonTest/java/ua/anironglass/boilerplate/utils/TestLifecycleFactory.java
+++ app/src/commonTest/java/ua/anironglass/boilerplate/utils/TestLifecycleFactory.java
@@ -1,15 +1,18 @@
 package ua.anironglass.boilerplate.utils;
 
+import android.support.annotation.NonNull;
+
 import com.trello.rxlifecycle.android.ActivityEvent;
 
 import rx.Observable;
 import rx.subjects.BehaviorSubject;
 
-public class TestLifecycleFactory {
+public final class TestLifecycleFactory {
 
     private static final BehaviorSubject<ActivityEvent> mLifecycleSubject =
             BehaviorSubject.create();
 
+    @NonNull
     public static Observable<ActivityEvent> getLifecycle() {
         return mLifecycleSubject.asObservable();
     }
